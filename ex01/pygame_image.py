1d8a0b5064156e77b67aa14ae74c3711eba2b7f8
c0b221327d <c0b221327d@edu.teu.ac.jp>
2023-04-18 17:40:53 +0900

    演習

--- ex01/pygame_image.py
+++ ex01/pygame_image.py
@@ -6,10 +6,13 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_fl=pg.transform.flip(bg_img,True,False)
     kk=pg.image.load("ex01/fig/3.png")
     kk_fl=pg.transform.flip(kk,True,False)
     kk_rt=pg.transform.rotozoom(kk_fl,10,1.0)
-    kk_lst=[kk_fl,kk_rt]
+    kk_rt_a=pg.transform.rotozoom(kk_fl,3,1.0)
+    kk_rt_b=pg.transform.rotozoom(kk_fl,6,1.0)
+    kk_lst=[kk_fl,kk_rt_a,kk_rt_b,kk_rt,kk_rt_b,kk_rt_a]
 
     tmr = 0
 
@@ -18,14 +21,14 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        x=tmr%1600
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img,[1600-x,0])
-        screen.blit(kk_lst[tmr%2],[300,200])
+        x=tmr%3200
+        screen.blit(bg_img, [800-x, 0])
+        screen.blit(bg_img_fl,[2400-x,0])
+        screen.blit(bg_img_fl,[-800-x,0])
+        screen.blit(kk_lst[tmr%4],[300,200])
 
         pg.display.update()
         clock.tick(100)
-
 
 if __name__ == "__main__":
     pg.init()
